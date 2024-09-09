4d36af8a6f169c20ff8ef01450dc1d18980f88e1
Namoral <thynamoral18@gmail.com>
2024-09-09 17:45:23 +0700

    allow * for CORS

--- back-end/index.mjs
+++ back-end/index.mjs
@@ -26,13 +26,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: ["http://localhost:8800"],
-    methods: "GET,HEAD,PUT,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(registerRoute);
 app.use(loginRoute);
--- front-end/src/services/apiClient.ts
+++ front-end/src/services/apiClient.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:3000",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default apiClient;
