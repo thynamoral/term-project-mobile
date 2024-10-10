461abfe9d0fb92caf45a36c3214f31f5f1c48655
Namoral Thy <thynamoral18@gmail.com>
2024-10-10 17:35:45 +0700

    last deployed!

--- front-end/capacitor.config.ts
+++ front-end/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'term-project-mobile',
-  webDir: 'dist'
+  appId: "io.ionic.starter",
+  appName: "TechHub",
+  webDir: "dist",
 };
 
 export default config;
